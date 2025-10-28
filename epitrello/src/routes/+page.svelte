beb94fdce2912720b774a40a980c293040d689a9
Oxyto <louis.de.macedo.ldm@gmail.com>
2025-10-28 20:07:21 +0100

    feat: demo front

---
+++ epitrello/src/routes/+page.svelte
@@ -0,0 +1,48 @@
+<div id="top-bar" class="flex items-center gap-4 bg-gray-800 p-4 text-white shadow-lg shadow-gray-400 sticky top-0">
+	<img id="epitrello-logo" src="/logos/epitrello-logo.png" alt="Epitrello Logo" />
+	<h1 id="title" class="select-none">EpiTrello</h1>
+	<a
+		id="login"
+		href="/login"
+		class="ml-auto select-none rounded bg-blue-600 px-4 py-2 hover:bg-blue-700">Login</a
+	>
+</div>
+<div id="content" class="flex h-screen flex-col items-center justify-start z-0">
+	<div
+		id="welcome-section"
+		class="h-128 mb-10 flex w-screen flex-row justify-center bg-gray-100 p-48 text-center"
+	>
+		<h1 class="size-128 select-none text-left font-mono text-5xl font-bold">
+			Capture, organize, and share your ideas !
+		</h1>
+		<img
+			id="welcome-image"
+			src="/illustrations/phone.png"
+			alt="Welcome Illustration"
+			class="mx-auto mt-6"
+		/>
+	</div>
+	<div id="features-section" class="mb-10 grid w-screen grid-rows-3 gap-16 p-16">
+		<div
+			class="feature-card border-l-16 w-1/4 rounded-xl border-amber-500 bg-white p-6 text-center shadow-lg"
+		>
+			<h2 class="mb-2 text-2xl font-semibold">Feature 1</h2>
+			<p>Describe the first amazing feature of Epitrello here.</p>
+		</div>
+		<div
+			class="feature-card border-l-16 w-1/4 rounded-xl border-amber-500 bg-white p-6 text-center shadow-lg"
+		>
+			<h2 class="mb-2 text-2xl font-semibold">Feature 2</h2>
+			<p>Describe the second amazing feature of Epitrello here.</p>
+		</div>
+		<div
+			class="feature-card border-l-16 w-1/4 rounded-xl border-amber-500 bg-white p-6 text-center shadow-lg"
+		>
+			<h2 class="mb-2 text-2xl font-semibold">Feature 3</h2>
+			<p>Describe the third amazing feature of Epitrello here.</p>
+		</div>
+	</div>
+	<div id="footer" class="w-full bg-gray-800 p-4 text-center text-white">
+		<p>&copy; 2025 Epitrello. All rights reserved.</p>
+	</div>
+</div>
