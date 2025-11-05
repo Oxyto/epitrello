9e7c05e82251f28515b9b28c6be1423376bab5a6
Oxyto <louis.de.macedo.ldm@gmail.com>
2025-11-05 17:57:09 +0100

    feat: user home

---
+++ epitrello/src/routes/u/[user_id]/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import UserSearchBar from './user_search_bar.svelte';
+
+    const { data } = $props();
+    console.log(data);
+</script>
+
+<UserSearchBar />
+<h1>User Page for ID: {data.user_id}</h1>
---
+++ epitrello/src/routes/u/[user_id]/user_search_bar.svelte
@@ -0,0 +1,11 @@
+<div id="user-search-bar" class="flex h-16 items-center gap-4 bg-gray-800 p-4 shadow-md">
+	<a href="/" class="text-white">EpiTrello</a>
+	<input type="text" placeholder="Search" class="flex-1 rounded bg-gray-700 px-4 py-2 text-white" />
+	<a href="#create" class="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600">Create</a>
+	<a href="#notifications" class="rounded bg-blue-600 px-4 py-2 text-white hover:bg-blue-700"
+		><img src="/path/to/notification/icon.svg" alt="Notifications" /></a
+	>
+	<a href="#profile" class="rounded bg-blue-600 px-4 py-2 text-white hover:bg-blue-700"
+		><img src="/path/to/profile/icon.svg" alt="Profile" /></a
+	>
+</div>
