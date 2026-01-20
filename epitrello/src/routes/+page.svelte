ec23b0ef259049b8f6ac120a8aaea8d51257ef07
Oxyto <louis.de.macedo.ldm@gmail.com>
2026-01-20 22:52:11 +0100

    fix: homepage

--- epitrello/src/routes/+page.svelte
+++ epitrello/src/routes/+page.svelte
@@ -1,24 +1,25 @@
 <script lang="ts">
 	import TopBar from './top_bar.svelte';
+	import Monitor from '$lib/assets/illustrations/monitor.png'
 </script>
 
 <TopBar login={false} />
-<div id="content" class="flex flex-col items-center justify-start z-0 font-['IBM Plex Sans, sans-serif']">
+<div id="content" class="items-center justify-start z-0">
 	<div
 		id="welcome-section"
-		class="flex w-screen flex-row h-[calc(100vh-7.5rem)] items-center justify-center bg-gray-100 p-48 text-center"
+		class="flex w-screen flex-row h-[calc(100vh-7.5rem)] overflow-hidden items-center justify-center bg-gray-100 text-center"
 	>
-		<h1 class="select-none text-center font-mono text-5xl font-bold">
+		<h1 class="select-none text-center font-mono text-5xl font-bold z-1 w-[50%]">
 			Welcome to EpiTrello !
 		</h1>
 		<img
 			id="welcome-image"
-			src="/illustrations/phone.png"
+			src={Monitor}
 			alt="Welcome Illustration"
-			class="mx-auto mt-6"
+			class="z-0 w-[60%]"
 		/>
 	</div>
-	<div id="footer" class="w-full bg-sky-950 p-4 text-center text-white select-none">
+	<div id="footer" class="sticky overflow-visible bottom-0 z-20 w-full bg-sky-950 p-4 text-center text-white select-none">
 		<p>&copy; 2025 Epitrello. All rights reserved.</p>
 	</div>
 </div>
